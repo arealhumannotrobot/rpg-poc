28a00b0cdefd348e0d2f174705c19d0234e349b2
Ashley <arealhumannotrobot@gmail.com>
2019-10-31 00:16:06 +0800

    added scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import pgp from 'openpgp';
 import QrReader from 'react-qr-reader';
 //=====================================//
