bb0356bcad40ca8f8a429a739271e186f02e8047
Ashley <arealhumannotrobot@gmail.com>
2019-11-03 16:30:26 +0800

    Redux init. fix act_creator return type

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,63 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import pgp from 'openpgp';
-import QrReader from 'react-qr-reader';
-// import {} from ''
-//=====================================//
-import {Nav, NavItem, Container} from 'react-bootstrap';
-import CompKeyInput from './comp/CompKeyInput';
+//=============React Bootstrap==================//
+import {Nav, Container} from 'react-bootstrap';
 import NavLink from 'react-bootstrap/NavLink';
+//==============React Redux=========//
+import {connect} from 'react-redux';
+import { INIT_STATE_TYPE } from './redux/initState';
+import { actcreate_tab_setTab } from './redux/reduce_tab';
 
-let refPub, refPvt, refMsg
+type AppProps = {
+  reduxState:INIT_STATE_TYPE,
+  actcreate_tab_setTab: typeof actcreate_tab_setTab,
+}
+const App: React.FC<AppProps> = (props) => {
 
-const App: React.FC = () => {
-  const [pubVal, setPubVal] = useState("");
-  const [pvtVal, setPvtVal] = useState("");
-  const [msgVal, setMsgVal] = useState("");
-
-  const [alias, setAlias] = useState("");
-  const [email, setEmail] = useState("");
-  const [passphrase, setPassphrase] = useState("");
-  const [passconfirm, setPassconfirm] = useState("");
-
-
-  const [keyThumb, setKeyThumb] = useState(<div></div>);
-
-  // const [showQRScan, setShowQRScan] = useState(true);
-  const [showQRScan, setShowQRScan] = useState(false);
-  const [qrCode, setQrCode] = useState("");
+  type RpgTabContentProps = {
+    reduxState:INIT_STATE_TYPE,
+  }
+  const RpgTabContent:React.FC<RpgTabContentProps> = (props) => {
+    return (
+      <div>
+        <p>{props.reduxState.tab}</p>
+      </div>
+    )
+  }
 
   return (
-
-
     <div className="App">
+      <p>
+        Code for Proof of Concept: 
+        <a href="https://github.com/arealhumannotrobot/rpg-poc">
+          https://github.com/arealhumannotrobot/rpg-poc
+        </a>
+      </p>
       <Container>
-        <Nav>
-          <NavLink>Hello</NavLink>
-          <NavLink>Hello</NavLink>
-          <NavLink>Hello</NavLink>
-          <NavLink>Hello</NavLink>
-          <NavLink>Hello</NavLink>
+        <Nav 
+          activeKey="MY_KEYS" 
+          onSelect={ (k:any) => {console.log(`selected ${k}`); props.actcreate_tab_setTab(k)}}
+        >
+          <NavLink eventKey="MY_KEYS">MY_KEYS</NavLink>
+          <NavLink eventKey="NEW_KEYS">NEW_KEYS</NavLink>
+          <NavLink eventKey="KEY_SIGNING">KEY_SIGNING</NavLink>
+          <NavLink eventKey="EXCH_KEY_RING">EXCH_KEY_RING</NavLink>
+          <NavLink eventKey="VERF_STRANGER">VERF_STRANGER</NavLink>
+          <NavLink eventKey="MSG_CRYPT">MSG_CRYPT</NavLink>
         </Nav>
+        <RpgTabContent
+          reduxState={props.reduxState}
+        />
       </Container>
 
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state:INIT_STATE_TYPE) => ({ reduxState: state });
+const mapDispatchToProps = {
+  actcreate_tab_setTab
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
--- src/redux/reduce_tab.tsx
+++ src/redux/reduce_tab.tsx
@@ -3,29 +3,28 @@ import { INIT_STATE, INIT_STATE_TYPE } from "./initState"
 //-----------------------------------
 export const ACT_TYPE_TAB_SET_TAB = 'ACT_TYPE_TAB_SET_TAB';
 type actIntent_tab_SetTab = {
-    actType: typeof ACT_TYPE_TAB_SET_TAB
+    type: typeof ACT_TYPE_TAB_SET_TAB
     tab: INIT_STATE_TYPE["tab"]
 }
 export const actcreate_tab_setTab = (tab:INIT_STATE_TYPE["tab"]):actIntent_tab_SetTab=>{
-    return { actType: ACT_TYPE_TAB_SET_TAB, tab };
+    return { type: ACT_TYPE_TAB_SET_TAB, tab };
 }
 
 //-----------------------------------
 export const ACT_TYPE_TAB_STUB = 'ACT_TYPE_TAB_STUB';
 type actIntent_tab_Stub = {
-    actType: typeof ACT_TYPE_TAB_STUB
+    type: typeof ACT_TYPE_TAB_STUB
     stub: string
 }
 export const actcreate_tab_stub = (stub:string):actIntent_tab_Stub=>{
-    return { actType: ACT_TYPE_TAB_STUB, stub };
+    return { type: ACT_TYPE_TAB_STUB, stub };
 }
 //===================================
+
+
 type actIntent_tab = actIntent_tab_SetTab|actIntent_tab_Stub;
-
-
-//===================================
 export function reduce_tab(state=INIT_STATE["tab"], action:actIntent_tab){
-    switch (action.actType) {
+    switch (action.type) {
         case ACT_TYPE_TAB_SET_TAB:
             return action.tab;
         case ACT_TYPE_TAB_STUB:
